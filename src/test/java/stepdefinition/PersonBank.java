3066372434ae61840bc7355689f6635721ed98aa
johnskpaul <jk.paul@pitt.edu>
2018-06-29 15:14:39 -0400

    cucumber files added

--- src/test/java/stepdefinition/PersonBank.java
+++ src/test/java/stepdefinition/PersonBank.java
@@ -1,170 +1,179 @@
 package stepdefinition;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class PersonBank {
+	
+	
 	@Given("PBTa The user is on personal home page")
 	public void pbta_The_user_is_on_personal_home_page() {
-	    // Write code here that turns the phrase above into concrete actions
-//	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// // throw new PendingException();
 	}
 
 	@Given("PBTa BANKING is present on the menu")
 	public void pbta_BANKING_is_present_on_the_menu() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTa cursor is placed on the BANKING menu")
 	public void pbta_cursor_is_placed_on_the_BANKING_menu() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTa the drop down appear with chcking link")
 	public void pbta_the_drop_down_appear_with_chcking_link() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTa the user click on Checking link")
 	public void pbta_the_user_click_on_Checking_link() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTa the user lands on Personal Checking Accounts page")
 	public void pbta_the_user_lands_on_Personal_Checking_Accounts_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTa Find the Right Checking Account button is present")
 	public void pbta_Find_the_Right_Checking_Account_button_is_present() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTa the user click on Find Right Checking Account button")
 	public void pbta_the_user_click_on_Find_Right_Checking_Account_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTa the user lands on questionnaire page")
 	public void pbta_the_user_lands_on_questionnaire_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTa First question is visible")
 	public void pbta_First_question_is_visible() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Given("PBTb The user is on personal home page")
 	public void pbtb_The_user_is_on_personal_home_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Given("PBTb BANKING is present on the menu")
 	public void pbtb_BANKING_is_present_on_the_menu() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTb cursor is placed on the BANKING menu")
 	public void pbtb_cursor_is_placed_on_the_BANKING_menu() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTb the drop down appear with Savings link")
 	public void pbtb_the_drop_down_appear_with_Savings_link() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTb the user click on Savings link")
 	public void pbtb_the_user_click_on_Savings_link() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTb the user lands on PNC savings page")
 	public void pbtb_the_user_lands_on_PNC_savings_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTb Find the right savings account is present")
 	public void pbtb_Find_the_right_savings_account_is_present() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Given("PBTc The user is on personal home page")
 	public void pbtc_The_user_is_on_personal_home_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Given("PBTc BANKING is present on the menu")
 	public void pbtc_BANKING_is_present_on_the_menu() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTc cursor is placed on the BANKING menu")
 	public void pbtc_cursor_is_placed_on_the_BANKING_menu() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTc the drop down appear with crdit cards link")
 	public void pbtc_the_drop_down_appear_with_crdit_cards_link() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTc the user click on Savings link")
 	public void pbtc_the_user_click_on_Savings_link() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTc the user lands on Personal Credit Cards")
 	public void pbtc_the_user_lands_on_Personal_Credit_Cards() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTc the Compare Our Credit Cards link is present")
 	public void pbtc_the_Compare_Our_Credit_Cards_link_is_present() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("PBTc the user click on Compare Credit Cards")
 	public void pbtc_the_user_click_on_Compare_Credit_Cards() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTc lands on Compare Credit Cards page")
 	public void pbtc_lands_on_Compare_Credit_Cards_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("PBTc Apply options are present")
 	public void pbtc_Apply_options_are_present() {
-	    // Write code here that turns the phrase above into concrete actions
-	    // throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
+	}
+
+	@After
+	public void after() {
+		System.out.println("Driver going to quit after PersonBank");
+
 	}
 }
