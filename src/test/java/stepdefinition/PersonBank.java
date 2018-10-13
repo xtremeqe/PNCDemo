28082f1c880f9ea0bb89b4730e75d94f2afb5ce7
XtremeQE <44072133+xtremeqe@users.noreply.github.com>
2018-10-13 10:54:15 -0400

    Update PersonBank.java

--- src/test/java/stepdefinition/PersonBank.java
+++ src/test/java/stepdefinition/PersonBank.java
@@ -5,8 +5,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.pnc.pncdemo.TestBase;
-
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
