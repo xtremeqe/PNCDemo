3fb3e46922ed99b6ebe785985fabb9ae7dd692ea
XtremeQE <44072133+xtremeqe@users.noreply.github.com>
2018-10-15 13:07:12 -0400

    Update TestRun.java

--- src/test/java/tests/TestRun.java
+++ src/test/java/tests/TestRun.java
@@ -35,7 +35,7 @@ public class TestRun  {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	
-	System.setProperty("webdriver.chrome.driver", "PNCdemo/chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "http://18.207.165.143/jenkins/job/Dots%20Test/ws/chromedriver.exe");
                 
 		
 		driver = new ChromeDriver();
